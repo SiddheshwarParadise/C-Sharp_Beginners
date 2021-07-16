74ed97bcf8ddb7c541b6e8967313eeced960580d
SiddheshwarParadise <77524511+SiddheshwarParadise@users.noreply.github.com>
2021-07-16 13:05:55 +0530

    Learning
    Just Started

---
+++ C-Sharp_Begginers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Begginers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow World!");
+            Console.ReadLine();
+        }
+    }
+}
